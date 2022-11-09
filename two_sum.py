b5b861d1d7ca96457ae9ea4b0bb1803271eeebe4
jplaulau14 <code.patricklaurel@gmail.com>
2022-11-09 22:19:49 +0800

    two pass hash table approach

--- two_sum.py
+++ two_sum.py
@@ -5,3 +5,14 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+# Two-pass Hash Table
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, num in enumerate(nums):
+            num_dict[num] = i
+            
+        for i, num in enumerate(nums):
+            if target - num in num_dict and num_dict[target - num] != i:
+                return [i, num_dict[target - num]]
